918285186f7d9da5bad8902e76a72a756c743fde
JacobTB99 <jb467917@ohio.edu>
2019-01-23 18:44:59 -0500

    Added program for lab

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Jacob Beauchamp
+ *        Date: January 23, 2019
+ * Description: Progarm for the second CS2400 Lab
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+cout << "Hello Git!!" << endl;
+    return 0;
+}// main
